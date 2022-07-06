07661fc0fd7107fd0cdd7351e707b00e9ebfc00e
JRodriS <107652616+JRodriS@users.noreply.github.com>
2022-07-05 21:04:57 -0300

    Add files via upload

---
+++ Desafio1Modulo004.java
@@ -0,0 +1,11 @@
+
+public class Desafio1Modulo004 {
+
+	public static void main(String[] args) {
+		for (int n = 1; n <= 10; n++){
+			 System.out.println(n);
+			}
+
+	}
+
+}
---
+++ Desafio2Modulo004.java
@@ -0,0 +1,15 @@
+
+public class Desafio2Modulo004 {
+
+	public static void main(String[] args) {
+		double dinero = 1000;
+		int mes = 1;
+		while (mes<=12) {
+		dinero = dinero * 1.02;
+		  mes++;
+		}
+		System.out.println(dinero);
+
+	}
+
+}
