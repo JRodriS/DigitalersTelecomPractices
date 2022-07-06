ab343753d5dffb452f14cafd028400a56a704ead
JRodriS <107652616+JRodriS@users.noreply.github.com>
2022-07-05 21:05:22 -0300

    Add files via upload

---
+++ Desafio3Modulo005.java
@@ -0,0 +1,28 @@
+
+public class Desafio3Modulo005 {
+
+	public static void main(String[] args) {
+		
+		double[] inflacion={0.8,0.1,0.3,0.4,0.3,0.6,0.5,0.3,0.7,0.3,0.2,0.9};
+		int mes_min=0,mes_max=0;
+		int n; double total=0,maximo=inflacion[0], minimo=inflacion[0];
+		
+		   for (n = 0; n < inflacion.length; n++){
+		    total = total + inflacion[n];
+		   if (inflacion[n] > maximo){
+		    maximo = inflacion[n];
+		    mes_max=n+1;
+		 }
+		  if (inflacion[n] < minimo){
+		    minimo = inflacion[n];
+		    mes_min=n+1;
+		 }
+		}
+		System.out.println("Promedio: " + total/12);
+		System.out.println("Minima inflacion: " + minimo+" mes:"+mes_min);
+		System.out.println("Maxima inflacion: " + maximo+" mes:"+mes_max);
+
+
+	}
+
+}
