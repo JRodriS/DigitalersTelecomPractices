239e14089db5d19e8a288973b766fef21e6bb6cd
JRodriS <107652616+JRodriS@users.noreply.github.com>
2022-07-05 21:04:20 -0300

    Add files via upload

---
+++ Desafio1Modulo003.java
@@ -0,0 +1,14 @@
+
+public class Desafio1Modulo003 {
+
+	public static void main(String[] args) {
+		int nro1 = 100,nro2 = 500, nro3 = 250;
+		  
+		 if (nro1 > nro2 & nro1 > nro3) { System.out.println(" el nro1 es el mayor: " + nro1); }
+		 if (nro2 > nro1 & nro2 > nro3) { System.out.println(" el nro2 es el mayor: " + nro2); }
+		 if (nro3 > nro2 & nro3 > nro1) { System.out.println(" el nro3 es el mayor: " + nro3); }
+
+
+	}
+
+}
---
+++ Desafio2Modulo003.java
@@ -0,0 +1,15 @@
+
+public class Desafio2Modulo003 {
+
+	public static void main(String[] args) {
+		
+		int a = 10, b = -2, c = 5;
+		
+		  if (a > 0 & b > 0) { System.out.println("a x b = " + a * b); }
+		  if (a > 0 & c > 0) { System.out.println("a x c = " + a * c); }
+		  if (b > 0 & c > 0) { System.out.println("b x c = " + b * c); }
+
+
+	}
+
+}
